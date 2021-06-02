ab9fa89753d4850cefd2f79e1afd47b10a906e67
AnkurApp <ankur.kumar@appinventiv.com>
2021-06-02 09:55:38 +0530

    Added New component Home

---
+++ src/modules/home/index.js
@@ -0,0 +1,19 @@
+// imports
+// component declare/define.
+// html
+
+import React, {Component} from 'react';
+
+// Class Component
+class Home extends Component {
+    render(){
+        return(
+            <div>
+                <h1>{"Hello from Home"}</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
+// export Home;
